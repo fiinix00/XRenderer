c3fe45fefbf9bc63aef9a832966806255bc0099a
Steven Hartgers <fiinix00@gmail.com>
2019-01-02 19:31:18 +0100

    Use syncronized clock timer and add cache directive for static data.

--- src/LitClock.tsx
+++ src/LitClock.tsx
@@ -1,6 +1,8 @@
 
 import { html, svg, SVGTemplateResult } from "lit-html";
 
+import { cache } from "lit-html/directives/cache";
+
 import XElement, { registerElement, uses, $, supportXType } from "./XElement";
 import cxsStyle from "./cxsstyle";
 
@@ -16,88 +18,105 @@ export default class LitClock extends XElement {
     get date() { return this._date; }
     set date(v) { this._date = v; this.dataChanged(); }
 
+    static interval: number = -1;
+    static clocks: LitClock[] = [];
+
+    static updateAllClocks() {
+        var date = new Date();
+
+        for (var i = 0; i < LitClock.clocks.length; i++) {
+            LitClock.clocks[i].date = date;
+        }
+    }
+
     constructor() {
         super();
+        
+        LitClock.clocks.push(this);
 
-        const updateDate = () => {
-            this.date = new Date();
-        };
-
-        setInterval(updateDate, 1000 / 1 /*fps*/); 
+        if (LitClock.interval === -1) {
+            setInterval(LitClock.updateAllClocks, 1000 / 1 /*fps*/); 
+        }
     }
 
     //<g transform='rotate(${ 6 * this.date.getSeconds()})'> 
     //+ this.date.getMilliseconds() / 165
 
+    //position: absolute;
+
     static hostStyle: string = `
-        :host { display: block; }
+        :host { display: inline; }
         svg {
-            position: absolute;
-            width: 100%;
-            height: 100%;
+            width: 33px;
+            height: 33px;
         }
     `;
 
     render() {
-        return $(
-            <host>
-                <style>
-                    ${cxsStyle()}
-                </style>
+        return $(<host>
+            <style>
+                ${cxsStyle()}
+            </style>
 
-                <style>
-                    ${LitClock.hostStyle}
-                </style>
+            <style>
+                ${LitClock.hostStyle}
+            </style>
 
-                <div class={square}>
+            <div class={square}>
 
-                    {/* so the SVG keeps its aspect ratio */}
-                    <svg viewBox='0 0 100 100'>
+                {/* so the SVG keeps its aspect ratio */}
+                <svg viewBox='0 0 100 100'>
 
-                        {/*first create a group and move it to 50,50 so all co-ords are relative to the center */}
-                        <g transform='translate(50,50)'>
-                            <circle class={clockFace} r='48' />
-                            ${minuteTicks}
-                            ${hourTicks}
+                    {/*first create a group and move it to 50,50 so all co-ords are relative to the center */}
+                    <g transform='translate(50,50)'>
+                        <circle class={clockFace} r='48' />
+                        ${minuteTicks}
+                        ${hourTicks}
 
-                            {/*hour hand*/}
-                            <line class={hour} y1='2' y2='-20'
-                                transform='rotate(${ 30 * this.date.getHours() + this.date.getMinutes() / 2})' />
+                        {/*hour hand*/}
+                        <line class={hour} y1='2' y2='-20'
+                            transform='rotate(${ 30 * this.date.getHours() + this.date.getMinutes() / 2})' />
 
-                            {/*minute hand*/}
-                            <line class={minute} y1='4' y2='-30'
-                                transform='rotate(${ 6 * this.date.getMinutes() + this.date.getSeconds() / 10})' />
+                        {/*minute hand*/}
+                        <line class={minute} y1='4' y2='-30'
+                            transform='rotate(${ 6 * this.date.getMinutes() + this.date.getSeconds() / 10})' />
 
-                            {/*second hand*/}
-                            <g transform='rotate(${ 6 * this.date.getSeconds() })'>
-                                <line class={secondColor} y1='10' y2='-38' />
-                                <line class='${secondColor} ${secondCounterWeight}' y1='10' y2='2' />
-                            </g>
+                        {/*second hand*/}
+                        <g transform='rotate(${ 6 * this.date.getSeconds() })'>
+                            <line class={secondColor} y1='10' y2='-38' />
+                            <line class='${secondColor} ${secondCounterWeight}' y1='10' y2='2' />
                         </g>
-                    </svg>
-                </div>
-            </host>)!;
+                    </g>
+                </svg>
+            </div>
+        </host>)!;
     }
 }
 
 const minuteTicks = (() => {
-    const lines = [] as SVGTemplateResult[];
+    const lines = [] as string[];
 
     for (let i = 0; i < 60; i++) {
-        lines.push(svg`<line class='${minor}' y1='42' y2='45' transform='rotate(${360 * i / 60})'/>`);
+        lines.push(`<line class='${minor}' y1='42' y2='45' transform='rotate(${360 * i / 60})'/>`);
     }
-
-    return lines;
+    
+    const joined = lines.join("");
+    const value = svg([joined] as unknown as TemplateStringsArray);
+    
+    return cache(value);
 })();
 
 const hourTicks = (() => {
-    const lines = [] as SVGTemplateResult[];
+    const lines = [] as string[];
 
     for (let i = 0; i < 12; i++) {
-        lines.push(svg`<line class='${major}' y1='32' y2='45' transform='rotate(${360 * i / 12})'/>`);
+        lines.push(`<line class='${major}' y1='32' y2='45' transform='rotate(${360 * i / 12})'/>`);
     }
 
-    return lines;
+    const joined = lines.join("");
+    const value = svg([joined] as unknown as TemplateStringsArray);
+
+    return cache(value);
 })();
 
 export const LitClockX = supportXType(LitClock);
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 
-import { html } from "lit-html";
+import { html, TemplateResult } from "lit-html";
 import XElement, { registerElement, $ } from "./XElement";
 
 import { TodoX } from "./Todo";
 import { LitClockX } from "./LitClock";
 import { DogListX } from "./DogList";
+import { cache } from "lit-html/directives/cache";
 
 @registerElement
 export class Main extends XElement {
@@ -16,8 +17,22 @@ export class Main extends XElement {
             <host>
                 <x type={TodoX}></x>
                 <x type={DogListX}></x>
-                <x type={LitClockX}></x>
+                ${allTheClocks}
             </host>
         )!;
     }
 }
+
+const allTheClocks = (function () {
+
+    const items = [] as TemplateResult[];
+
+    for (var i = 0; i < 5; i++) {
+        items.push(
+            $(<div style="float:left;"><x type={LitClockX}></x></div>)!!
+        );
+    }
+
+    return cache(items);
+
+})();
